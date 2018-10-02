d92d2ee21b04ce0f2fcf1d9254728ffcda6f8e91
yzjing <jingyz42@gmail.com>
2018-10-02 17:39:01 -0400

    rearrange files; add code to handle coreference

---
+++ exp/20180827_event_seq/runNLPserver.sh
@@ -0,0 +1,2 @@
+cd /Users/a307/Desktop/work/current/narrativity/tools/stanford-corenlp-full-2016-10-31
+java  --add-modules java.se.ee -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
